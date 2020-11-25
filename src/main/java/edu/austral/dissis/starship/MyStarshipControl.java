1dcc126f8986ba2d31974d7b022ca28266f08059
sofiasidanez <sofia.sidanez@ing.austral.edu.ar>
2020-11-25 18:30:16 -0300

    now spaceship control supports shooting and moving at the same time

--- src/main/java/edu/austral/dissis/starship/MyStarshipControl.java
+++ src/main/java/edu/austral/dissis/starship/MyStarshipControl.java
@@ -13,27 +13,27 @@ public class MyStarshipControl extends Control {
 
         if (keySet.contains(controlConfiguration.getMoveFowardKey())) {
             movable =(StarshipInterface) movable.moveForward(2);
-            return movable;
+
         }
 
         if (keySet.contains(controlConfiguration.getMoveBackwardsKey())) {
             movable = (StarshipInterface) movable.moveBackwards(2);
-            return movable;
+
         }
 
         if (keySet.contains(controlConfiguration.getRotateLeftKey())) {
             movable = (StarshipInterface) movable.rotate(-1 * PConstants.PI / 60);
-            return movable;
+
         }
 
         if (keySet.contains(controlConfiguration.getRotateRightKey())) {
             movable = (StarshipInterface) movable.rotate(PConstants.PI / 60);
-            return movable;
+
         }
         if (keySet.contains(controlConfiguration.getShootkey())) {
             movable = (StarshipInterface) movable.shoot();
             keySet.remove(controlConfiguration.getShootkey());
-            return movable;
+
         }
         return movable;
 
